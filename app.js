f16c3f5d26dbcc50f4e0cc452524e59cfd54d68a
Michael Owolabi <michael.i.owolabi@gmail.com>
2020-01-26 14:18:22 +0100

    initial app setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('', (req, res) => {
+  res.send('<h2>Welcome to Ven10 Api</h2>');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
