d80955054ca31d8c3f153ca2b0a67e1229b3bf26
Michael Owolabi <michael.i.owolabi@gmail.com>
2020-01-28 00:52:06 +0100

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const routes = require('./src/routes/index');
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
